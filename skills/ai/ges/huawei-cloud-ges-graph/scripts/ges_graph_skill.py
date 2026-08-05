4a76b265c1380cace2a5a1d387783f1e8fba743b
changhui123456 <changhui123456@users.noreply.github.com>
2026-08-05 15:22:46 +0800

    update skill: ai/ges/huawei-cloud-ges-graph
    Co-authored-by: lccode<akuvwA9g6F7X8MUd.lccode@noreply.gitcode.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !131 merge main into main
    
    update skill: ai/ges/huawei-cloud-ges-graph
    
    Created-by: lccode
    Commit-by: lccode
    Merged-by: check_bot
    Description: 测试提交
    第二次修改
    Merge branch 'main' of gitcode.com:lccode/huaweicloud-skills-dev
    
    See merge request: developer-skill/huaweicloud-skills-dev!131

--- skills/ai/ges/huawei-cloud-ges-graph/scripts/ges_graph_skill.py
+++ skills/ai/ges/huawei-cloud-ges-graph/scripts/ges_graph_skill.py
@@ -12,9 +12,9 @@ import time
 import requests
 from typing import Dict, List, Optional, Any, Union
 
-# 项目根目录
+# 项目根目录-测试修改
 CURRENT_FILE = os.path.abspath(__file__)
-SKILL_DIR = os.path.dirname(os.path.dirname(CURRENT_FILE))  # skill根目录（SKILL.md所在目录）
+SKILL_DIR = os.path.dirname(os.path.dirname(CURRENT_FILE))  # skill根目录（SKILL.md所在目录）测试提交22222
 SCRIPTS_DIR = os.path.dirname(CURRENT_FILE)                  # scripts/
 SKILLS_DIR = os.path.dirname(SKILL_DIR)     # skills
 CODEMATE_DIR = os.path.dirname(SKILLS_DIR)  # .codemate
